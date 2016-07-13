b6b25f7fc5e882b3202d40714f0878177e4ec189
陈永盛 <chenyongsheng@xinli001.com>
2016-07-13 16:43:01 +0800

    项目初始化

---
+++ youyiku/youyiku/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  youyiku
+//
+//  Created by yixinli on 16/7/13.
+//  Copyright © 2016年 haojiyouxian. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
